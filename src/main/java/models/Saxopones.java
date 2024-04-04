ea409a264fa2c64874fe2900710f3067679243fb
nicholasmcnamra <nikomcnamara@gmail.com>
2024-04-04 18:20:39 -0400

    added saxophones properties

--- src/main/java/models/Saxopones.java
+++ src/main/java/models/Saxopones.java
@@ -1,4 +1,10 @@
 package models;
 
 public class Saxopones {
+    private int id;
+    private String brand;
+    private String type;
+    private String model;
+    private int quantity;
+    private float price;
 }
