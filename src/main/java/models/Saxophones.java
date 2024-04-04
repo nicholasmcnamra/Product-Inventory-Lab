b805f11e066f68e5f99f44071ffc7f54e2617437
nicholasmcnamra <nikomcnamara@gmail.com>
2024-04-04 18:51:57 -0400

    added saxophones getters/setters tests

---
+++ src/main/java/models/Saxophones.java
@@ -0,0 +1,58 @@
+package models;
+
+public class Saxophones {
+    private int id;
+    private String manufacturer;
+    private String type;
+    private String model;
+    private int quantity;
+    private double price;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
--- src/test/java/models/SaxophonesTest.java
+++ src/test/java/models/SaxophonesTest.java
@@ -1,4 +1,87 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
 public class SaxophonesTest {
+
+    @Test
+    public void setModelTest() {
+        //given (1)
+        String expected = "AWO10";
+
+        //when (2)
+        Saxophones testSaxophone = new Saxophones();
+        testSaxophone.setModel(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testSaxophone.getModel());
+    }
+
+    @Test
+    public void setIdTest() {
+        //given (1)
+        int expected = 27190345;
+
+        //when (2)
+        Saxophones testSaxophone = new Saxophones();
+        testSaxophone.setId(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testSaxophone.getId());
+    }
+
+    @Test
+    public void setTypeTest() {
+        //given (1)
+        String expected = "Alto";
+
+        //when (2)
+        Saxophones testSaxophone = new Saxophones();
+        testSaxophone.setType(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testSaxophone.getType());
+    }
+
+    @Test
+    public void setManufacturerTest() {
+        //given (1)
+        String expected = "Yanagisawa";
+
+        //when (2)
+        Saxophones testSaxophone = new Saxophones();
+        testSaxophone.setManufacturer(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testSaxophone.getManufacturer());
+    }
+
+    @Test
+    public void setQuantityTest() {
+        //given (1)
+        int expected = 8;
+
+        //when (2)
+        Saxophones testSaxophone = new Saxophones();
+        testSaxophone.setQuantity(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testSaxophone.getQuantity());
+    }
+
+    @Test
+    public void setPriceTest() {
+        //given (1)
+        double expected = 5839.0;
+
+        //when (2)
+        Saxophones testSaxophone = new Saxophones();
+        testSaxophone.setPrice(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testSaxophone.getPrice(), .0001);
+    }
+
 }
+
