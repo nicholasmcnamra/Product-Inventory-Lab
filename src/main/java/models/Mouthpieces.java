833c76ad4292fd5d9daa1a1574dd4d229aa57898
nicholasmcnamra <nikomcnamara@gmail.com>
2024-04-04 21:28:11 -0400

    added tests for mouthpieces

--- src/main/java/models/Mouthpieces.java
+++ src/main/java/models/Mouthpieces.java
@@ -1,4 +1,68 @@
 package models;
 
 public class Mouthpieces {
+
+    private int id;
+    private String manufacturer;
+    private String type;
+    private String material;
+    private String model;
+    private int quantity;
+    private double price;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
--- src/test/java/models/MouthpiecesTest.java
+++ src/test/java/models/MouthpiecesTest.java
@@ -1,4 +1,98 @@
 package models;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class MouthpiecesTest {
+
+    @Test
+    public void setModelTest() {
+        //given (1)
+        String expected = "Concept";
+
+        //when (2)
+        Mouthpieces testMouthpiece = new Mouthpieces();
+        testMouthpiece.setModel(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testMouthpiece.getModel());
+    }
+
+    @Test
+    public void setIdTest() {
+        //given (1)
+        int expected = 37204838;
+
+        //when (2)
+        Mouthpieces testMouthpiece = new Mouthpieces();
+        testMouthpiece.setId(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testMouthpiece.getId());
+    }
+
+    @Test
+    public void setTypeTest() {
+        //given (1)
+        String expected = "Soprano";
+
+        //when (2)
+        Mouthpieces testMouthpiece = new Mouthpieces();
+        testMouthpiece.setType(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testMouthpiece.getType());
+    }
+
+    @Test
+    public void setMaterialTest() {
+        //given (1)
+        String expected = "Rubber";
+
+        //when (2)
+        Mouthpieces testMouthpiece = new Mouthpieces();
+        testMouthpiece.setMaterial(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testMouthpiece.getMaterial());
+    }
+
+    @Test
+    public void setManufacturerTest() {
+        //given (1)
+        String expected = "Selmer";
+
+        //when (2)
+        Mouthpieces testMouthpiece = new Mouthpieces();
+        testMouthpiece.setManufacturer(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testMouthpiece.getManufacturer());
+    }
+
+    @Test
+    public void setQuantityTest() {
+        //given (1)
+        int expected = 75;
+
+        //when (2)
+        Mouthpieces testMouthpiece = new Mouthpieces();
+        testMouthpiece.setQuantity(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testMouthpiece.getQuantity());
+    }
+
+    @Test
+    public void setPriceTest() {
+        //given (1)
+        double expected = 158.84;
+
+        //when (2)
+        Mouthpieces testMouthpiece = new Mouthpieces();
+        testMouthpiece.setPrice(expected);
+
+        //then (3)
+        Assert.assertEquals(expected, testMouthpiece.getPrice(), .0001);
+    }
 }
